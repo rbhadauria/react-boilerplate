cf7e84072e340624d2a8e06bad47d4332031b1c1
rbhadauria <rbhadauria>
2020-01-19 14:22:38 +0530

    Added react-axe to determine accessibility issues

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import DefaultErrorBoundary from './DefaultErrorBoundary';
 import '@babel/polyfill';
 import App from './App';
 
+if (process.env.NODE_ENV === 'development') {
+  let axe = require('react-axe');
+  axe(React, ReactDOM, 1000);
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <DefaultErrorBoundary>
