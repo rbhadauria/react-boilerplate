5510486f674a45d289673d122c29c1440010c50c
rbhadauria <rbhadauria>
2020-01-19 12:05:10 +0530

    Hot module reloading enabled

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import  {hot}  from "react-hot-loader";
+import './index.css'
+class App extends React.Component{
+    state={
+        count:0
+    }
 
-const App = ()=>{
-    return (
-    <div>Hello Bhaddo !!</div>
+    render() {
+    return(
+    <div>
+        <div>Hello World !</div>
+        <h2
+        className={this.state.count>10?'warning':null}
+        >Count: {this.state.count}</h2>
+        <button onClick={()=>this.setState(state=>({count:state.count+1}))}>+</button>
+        <button onClick={()=>this.setState(state=>({count:state.count-1}))}>-</button>
+    </div>
     )
+    }
 }
-export default App;
+export default hot(module)(App);
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,6 +5,7 @@ module.exports = merge(baseConfig,{
     mode:'development',
     devServer:{
         port:3000
-    }
+    },
+    devtool:'source-map'
     
 })
