8208f1c711d33a66058cb48dd7b9bc3f994708c2
rbhadauria <rbhadauria>
2020-01-19 12:32:10 +0530

    Added webpack-bundle-analyzer and added support for selected browsers using babel/polyfill

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,14 @@
 const merge = require('webpack-merge');
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
 const baseConfig  = require('./webpack.config.base');
 
 module.exports = merge(baseConfig,{
-    mode:'production'
+    mode:'production',
+    plugins:[new BundleAnalyzerPlugin({
+        analyzerMode:'static'
+    })],
+    externals:{
+        react:'React',
+        "react-dom":'ReactDOM'
+    }
 })
