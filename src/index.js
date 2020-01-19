8b895e60b751c530c8aa44cb65204b40dff29150
rbhadauria <rbhadauria>
2020-01-19 11:38:13 +0530

    seperate webpack config for dev and prod, merge using webpack-merge, webpack-dev-server with live reload

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-import getGreeting from './greet';
-console.log(getGreeting('Rahul'))
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+
+
+ReactDom.render(<App/>,document.getElementById('root'));
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,7 +1,7 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports={
     entry:'./src/index.js',
-    mode:'production',
     output:{
         path:path.join(__dirname,'dist'),
         filename:'app.bundle.js'
@@ -13,9 +13,12 @@ module.exports={
                 loader:'babel-loader',
                 exclude:/node_modules/,
                 options:{
-                    presets:['@babel/preset-env']
+                    presets:['@babel/preset-env','@babel/preset-react']
                 }
             }
         ]
-    }
+    },
+    plugins:[new HtmlWebpackPlugin({
+        template:'./src/index.html'
+    })]
 }
